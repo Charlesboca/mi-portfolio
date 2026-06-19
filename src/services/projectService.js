dd31a8ce9ec8a0291bca01cbd886358d2b8d9c44
Charlesboca <carlos1ariel@hotmail.com>
2026-06-19 10:18:20 -0300

    sumado de tecnologias

--- src/services/projectService.js
+++ src/services/projectService.js
@@ -6,7 +6,7 @@ export const getProyectos = () => {
       id: 1,
       title: "Prode Futbolero",
       description: "App de pronósticos deportivos en tiempo real con optimización de lectura en Firebase.",
-      tech: ["React", "Firebase", "Vite"],
+      tech: ["React", "Firebase", "Vite","Vercel"],
       link: "https://prode-futbolero.vercel.app/",
       image: "/prode.jpg" // Agrega esta línea para incluir la imagen del proyecto
     },
@@ -14,7 +14,7 @@ export const getProyectos = () => {
       id: 2,
       title: "RowaSound",
       description: "Plataforma de servicios y catálogo profesional de Audio Car con enfoque corporativo.",
-      tech: ["React", "CSS", "Vite"],
+      tech: ["React", "CSS", "Vite","Video"],
       link: "https://www.rowasound.com.ar/",
       image: "/rowasound.jpg" // Agrega esta línea para incluir la imagen del proyecto
     },
@@ -22,7 +22,7 @@ export const getProyectos = () => {
       id: 3,
       title: "Llevate Todo Corrientes",
       description: "Plataforma de gestión de pedidos y e-commerce dinámico de productos varios.",
-      tech: ["React", "CSS","Firebase"],
+      tech: ["React", "CSS","Firebase","CRUD"],
       link: "https://www.llevatetodocorrientes.com.ar/",
       image: "/llevate-todo.jpg" // Agrega esta línea para incluir la imagen del proyecto
     }
